b7589efff52beedd543cb5e28152f37f77b8dd1c
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-07-23 11:45:38 +0100

    Update JSX todo list

--- src/component/todoList/TodoList.tsx
+++ src/component/todoList/TodoList.tsx
@@ -9,38 +9,20 @@ const TodoList = ({ todos }: Props) => {
   return (
     <ul className="list">
       <li className="list__item">
-        <input
-          className="list__item__input"
-          type="checkbox"
-          id="todo1"
-          name="todo11"
-          value="todo111"
-        />
-        <label className="list__item__label" htmlFor="todo1">
+        <label className="list__item__label">
+          <input className="list__item__input" type="checkbox" name="todo1" />
           make a fake todo
         </label>
       </li>
       <li className="list__item">
-        <input
-          className="list__item__input"
-          type="checkbox"
-          id="todo2"
-          name="todo22"
-          value="todo222"
-        />
-        <label className="list__item__label" htmlFor="todo2">
+        <label className="list__item__label">
+          <input className="list__item__input" type="checkbox" name="todo2" />
           add checked or not
         </label>
       </li>
       <li className="list__item">
-        <input
-          className="list__item__input"
-          type="checkbox"
-          id="todo3"
-          name="todo33"
-          value="todo333"
-        />
-        <label className="list__item__label" htmlFor="todo3">
+        <label className="list__item__label">
+          <input className="list__item__input" type="checkbox" name="todo3" />
           style todo
         </label>
       </li>
